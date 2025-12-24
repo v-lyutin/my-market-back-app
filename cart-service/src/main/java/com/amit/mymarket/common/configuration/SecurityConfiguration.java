05e84a954383bffa6896181efda69b29cec57455
Vladislav Lyutin <dyson-sv25@yandex.ru>
2025-12-24 18:01:12 +0300

    feat: configure security filter chain with form login and access rules

--- cart-service/src/main/java/com/amit/mymarket/common/configuration/SecurityConfiguration.java
+++ cart-service/src/main/java/com/amit/mymarket/common/configuration/SecurityConfiguration.java
@@ -2,18 +2,44 @@ package com.amit.mymarket.common.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.SecurityContextServerLogoutHandler;
+import org.springframework.security.web.server.authentication.logout.WebSessionServerLogoutHandler;
 
 @Configuration
 @EnableWebFluxSecurity
 @EnableMethodSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
+                        .pathMatchers(HttpMethod.GET, "/", "/items/**").permitAll()
+                        .pathMatchers(HttpMethod.POST, "/items/**").authenticated()
+                        .pathMatchers("/management/**").authenticated()
+                        .pathMatchers("/cart/**", "/orders/**").authenticated()
+                        .anyExchange().denyAll()
+                )
+                .formLogin(Customizer.withDefaults())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutHandler(new SecurityContextServerLogoutHandler())
+                        .logoutHandler(new WebSessionServerLogoutHandler())
+                )
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
+    }
 
     @Bean
     public MapReactiveUserDetailsService userDetailsService(PasswordEncoder encoder) {
